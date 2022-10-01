a8fd50e8c0123eb6128578ebbb52ac6ca48817d8
Abdul Saboor <abdulsaboor938@gmail.com>
2022-10-01 16:52:26 +0000

    browser

--- token.c
+++ token.c
@@ -1,13 +1,47 @@
-#include <unistd.h>
-#include <string.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
-int main()
-{
-    char *temp[] = {"echo", "hello"};
-    char *temp1 = {"world"};
-    *(temp + 2) = temp1;
-    *(temp + 3) = NULL;
-    execvp(temp[0], temp);
+char lastdir[PATH_MAX];  // initialized to zero
+
+int exec_cd(char *arg) {
+    char curdir[PATH_MAX];
+    char path[PATH_MAX];
+
+    if (getcwd(curdir, sizeof curdir)) {
+        /* current directory might be unreachable: not an error */
+        *curdir = '\0';
+    }
+    if (arg == NULL) {
+        arg = getenv("HOME");
+    }
+    if (!strcmp(arg, "-")) {
+        if (*lastdir == '\0') {
+            fprintf(stderr, "no previous directory\n");
+            return 1;
+        }
+        arg = lastdir;
+    } else {
+        /* this should be done on all words during the parse phase */
+        if (*arg == '~') {
+            if (arg[1] == '/' || arg[1] == '\0') {
+                snprintf(path, sizeof path, "%s%s", getenv("HOME"), arg + 1);
+                arg = path;
+            } else {
+                /* ~name should expand to the home directory of user with login `name` 
+                   this can be implemented with getpwent() */
+                fprintf(stderr, "syntax not supported: %s\n", arg);
+                return 1;
+            }
+        }
+    }
+    if (chdir(arg)) {
+        fprintf(stderr, "chdir: %s: %s\n", strerror(errno), path);
+        return 1;
+    }
+    strcpy(lastdir, curdir);
     return 0;
 }
